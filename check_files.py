5d21c892dd821935d087d3920c00ef109950a04f
Cole Williams <Cole@Coles-MacBook-Pro.local>
2019-12-11 16:48:56 -0700

    Changed version num

--- check_files.py
+++ check_files.py
@@ -4,7 +4,7 @@ import sys
 def start_up(parameter_dict):
 
     tree = ["\n           P O N D E R O S A",
-    "                v.1.1          ",
+    "                v.1.2          ",
     "                  &",                   
     "                 ##&",                   
     "                #####",                  
